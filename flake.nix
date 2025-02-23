9a6494ba19db05650dd709d0069e62995c73f29b
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-02-23 17:17:56 +0800

    Flake support

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A simple flake for VSCode theme development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "nordfox-vsc";
+
+          packages = with pkgs; [
+            nodejs
+            pnpm
+          ];
+        };
+      }
+    );
+}
