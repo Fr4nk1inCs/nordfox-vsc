e58adbac150305d279e508aaa60a5fe771844351
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-03-10 16:54:33 +0800

    build: git pre-commit hooks
    - prettier
    - commitizen

--- flake.nix
+++ flake.nix
@@ -4,12 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    hooks.url = "github:cachix/git-hooks.nix";
+
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
+    self,
     nixpkgs,
     utils,
+    hooks,
     ...
   }:
     utils.lib.eachDefaultSystem (
@@ -18,14 +22,29 @@
           inherit system;
         };
       in {
+        checks = {
+          pre-commit = hooks.lib.${system}.run {
+            src = ./.;
+
+            hooks = {
+              commitizen.enable = true;
+              prettier.enable = true;
+            };
+          };
+        };
         devShells.default = pkgs.mkShell {
           name = "nordfox-vsc";
 
-          packages = with pkgs; [
-            nodejs
-            pnpm
-          ];
+          inherit (self.checks.${system}.pre-commit) shellHook;
+          packages = with pkgs;
+            [
+              nodejs
+              pnpm
+            ]
+            ++ self.checks.${system}.pre-commit.enabledPackages;
         };
       }
     );
 }
+
+
